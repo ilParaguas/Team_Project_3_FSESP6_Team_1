51bb139e46dec5a343447e2e3dab858319a777dc
Carlos Cuartero Tabuenca <cuarterodosmilocho@gmail.com>
2024-03-09 10:42:36 +0100

    feature/footer/termsEA: CSS with media queries

--- EA/src/home_page/jsx/Components/footer/Footer.jsx
+++ EA/src/home_page/jsx/Components/footer/Footer.jsx
@@ -1,19 +1,15 @@
-import { useContext } from "react";
 import { JsonContext } from "/src/home_page/jsx/Contexts/FooterJson.jsx"
-import { LanguageContext } from "/src/home_page/jsx/Contexts/Language-Context.jsx"
 import { TermsEA } from "./TermsEA";
 import { MediaLinks } from "./MediaLinks";
 import { ServsAndLegalTerms } from "./ServsAndLegalTerms";
 import { useFooterJsonFetch } from "../../Hooks/useFooterJsonFetch";
 
 export function Footer() {
-    const lang = useContext(LanguageContext);
     const json = useFooterJsonFetch()
 
     return (
-        <div>
+        <div className="footer">
             <JsonContext.Provider value={json}>
-                {lang}
                 <TermsEA />
                 <MediaLinks />
                 <ServsAndLegalTerms />
--- EA/src/home_page/jsx/Components/footer/TermsEA.jsx
+++ EA/src/home_page/jsx/Components/footer/TermsEA.jsx
@@ -4,7 +4,9 @@ import { JsonContext } from "/src/home_page/jsx/Contexts/FooterJson.jsx";
 export function TermsEA() {
     const data = useContext(JsonContext);
 
-    return <div>
-       {data && <p>{data["terms-ea"].start}<strong><a>{data["terms-ea"].link}</a></strong>{data["terms-ea"].final}</p>}
+    return <div className="terms-EA">
+        {data && <p className="terms-EA-p">{data["terms-ea"].start}
+        <a href="">{data["terms-ea"].link}</a>
+        {data["terms-ea"].final}</p>}
     </div>
 }
