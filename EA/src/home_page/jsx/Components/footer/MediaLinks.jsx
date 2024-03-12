31dfaf8e498a63f4284231b87820761ff8b9637c
Carlos Cuartero Tabuenca <cuarterodosmilocho@gmail.com>
2024-03-12 17:24:00 +0100

    fix(footer): media query problem with flex display and a border

--- EA/src/home_page/jsx/Components/footer/MediaLinks.jsx
+++ EA/src/home_page/jsx/Components/footer/MediaLinks.jsx
@@ -8,25 +8,15 @@ export function MediaLinks() {
     <div id="media-links">
         {data && <section>
             <div id="local-links">{data["media-links"]["local-links"].map(link => {
-                return <InfoDiv key={link} link={link}/>;
+                return <div key={link} className="local-link" title={link}><a href="">{link}</a></div>;
             })}</div>
             <div id="social-container">
                 <h3 id="social-links-h3">{data["media-links"].join}</h3>
                 <div id="social-links">{data["media-links"]["social-links"].map(link => {
-                    return <NetworkDiv key={link} link={link} />
+                    return <div key={link} className="social-link" title={link}><a href=""><img src={`./src/home_page/media/img/footer/social/${link}.svg`} alt={link} /></a></div>
                 })}</div>
             </div>
         </section>}
     </div>
     );
-}
-
-function InfoDiv({ link }) {
-
-    return <div className="local-link" title={link}><a href="">{link}</a></div>;
-}
-
-function NetworkDiv({ link }) {
-
-    return <div className="social-link" title={link}><a href=""><img src={`./src/home_page/media/img/footer/social/${link}.svg`} alt={link} /></a></div>
 }
