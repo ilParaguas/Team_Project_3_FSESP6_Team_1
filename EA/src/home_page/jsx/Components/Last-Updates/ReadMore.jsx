ede3068af9d2eb61e45c1f2501cdd4c3a008f760
Christian <christianalexp@live.com>
2024-03-14 19:46:17 +0100

    Task13

---
+++ EA/src/home_page/jsx/Components/Last-Updates/ReadMore.jsx
@@ -0,0 +1,10 @@
+export function ReadMore() {
+  return (
+    <div id="read-more">
+      <a id="read-more-link" href="#read-more">
+        Leer mas
+      </a>
+      <span id="read-more-text">Leer mas</span>
+    </div>
+  );
+}
