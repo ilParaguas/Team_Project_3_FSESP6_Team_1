53d930a6cb17295a5e2e5843176e5d37b1c12ccd
Monica Guerra <monik_flute@hotmail.com>
2024-03-26 14:41:19 +0100

    elaboracion del componente para botones e incorporacion al codigo

--- EA/src/home_page/jsx/Components/Play-Sections/ZonaCeleste.jsx
+++ EA/src/home_page/jsx/Components/Play-Sections/ZonaCeleste.jsx
@@ -1,28 +1,29 @@
 import { PlaySectionsContext } from "../../Contexts/PlaySectionsContext";
 import { useContext } from "react";
+import { PlaySecButtons } from "./Play-S-Button";
 
 export default function ZonaCeleste() {
   const data = useContext(PlaySectionsContext);
 
   return (
     <>
-    {data &&
-    <div id="zona-celeste" className="zona-celeste">
-      <img
-        id="imgZonaCeleste"
-        src="./src/home_page/media/Play-Sections/celeste.1920w.jpg"
-        className="imgZonaCeleste"
-      />
-      <div id="zona-celeste-contenido" className="zona-celeste-contenido">
-        <p id="parrafoCeleste" className="parrafoCeleste">
-         {data.parrafoCeleste}
-        </p>
-        <button id="botonInfoCeleste" className="botonInfoCeleste">
-          {data.botonInfoCeleste}
-        </button>
-      </div>
-    </div>
-}
+      {data && (
+        <div id="zona-celeste" className="zona-celeste">
+          <img
+            id="imgZonaCeleste"
+            src="./src/home_page/media/Play-Sections/celeste.1920w.jpg"
+            className="imgZonaCeleste"
+          />
+          <div id="zona-celeste-contenido" className="zona-celeste-contenido">
+            <p id="parrafoCeleste" className="parrafoCeleste">
+              {data.parrafoCeleste}
+            </p>
+            <PlaySecButtons
+              texto={data.botonInfoCeleste}
+              />
+          </div>
+        </div>
+      )}
     </>
   );
 }
--- EA/src/home_page/jsx/Components/Play-Sections/ZonaRoja.jsx
+++ EA/src/home_page/jsx/Components/Play-Sections/ZonaRoja.jsx
@@ -1,39 +1,41 @@
 import { PlaySectionsContext } from "../../Contexts/PlaySectionsContext";
 import { useContext } from "react";
+import { PlaySecButtons } from "./Play-S-Button";
 
 export default function ZonaRoja() {
   const data = useContext(PlaySectionsContext);
 
   return (
     <>
-    {data &&
-    <div className="zona-roja">
-      <img
-        id="imgZonaRoja"
-        src="./src/home_page/media/Play-Sections/recuadros7x2.1920w.jpg"
-        className="imgZonaRoja"
-      />
-      <img
-        id="imgZonaRojaGradient"
-        src="./src/home_page/media/Play-Sections/gradient-azul7x2.1920w.png"
-        className="imgZonaRojaGradient"
-      />
-      <div id="play-container" className="play-container">
-        <img
-          id="playLogo"
-          src="./src/home_page/media/Play-Sections/play-log-big.svg"
-          alt="play Logo"
-          className="playLogo"
-        />
-        <p id="parrafoPlay" className="parrafoPlay">
-          {data.parrafoPlay}
-        </p>
-        <button id="botonUneteYa" className="botonUneteYa">
-          {data.botonUneteYa}
-        </button>
-      </div>
-    </div>
-}
-</>
+      {data && (
+        <div className="zona-roja">
+          <img
+            id="imgZonaRoja"
+            src="./src/home_page/media/Play-Sections/recuadros7x2.1920w.jpg"
+            className="imgZonaRoja"
+          />
+          <img
+            id="imgZonaRojaGradient"
+            src="./src/home_page/media/Play-Sections/gradient-azul7x2.1920w.png"
+            className="imgZonaRojaGradient"
+          />
+          <div id="play-container" className="play-container">
+            <img
+              id="playLogo"
+              src="./src/home_page/media/Play-Sections/play-log-big.svg"
+              alt="play Logo"
+              className="playLogo"
+            />
+            <p id="parrafoPlay" className="parrafoPlay">
+              {data.parrafoPlay}
+            </p>
+            <div>
+            <PlaySecButtons
+            texto={data.botonUneteYa}/>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
