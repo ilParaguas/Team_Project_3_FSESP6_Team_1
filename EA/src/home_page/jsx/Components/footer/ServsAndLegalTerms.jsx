4973f82bca6e0bbca508d821475c51666b542571
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-04-08 19:21:29 +0200

    Fix parentesis en footer

--- EA/src/home_page/jsx/Components/footer/ServsAndLegalTerms.jsx
+++ EA/src/home_page/jsx/Components/footer/ServsAndLegalTerms.jsx
@@ -7,19 +7,39 @@ export function ServsAndLegalTerms() {
   const data = useContext(JsonContext);
   const lang = useContext(LanguageContext);
 
-    return <div id="servs-and-legal">
-        {data && <section id="servs-and-legal-section">
-            <div><a href="" title={data["servs-and-legal-terms"]["ea-icon"]}><img id="ea-violet" src="./src/home_page/media/img/footer/ea-violet.png" alt="ea-logo" /></a></div>
-            <div id="ea-servs-and-legal">
-                <div>
-                    <div id="servs-links">{data["servs-and-legal-terms"].servs.map(link => ( 
-                        <div className="servs-link" key={link}><a href="">{link}</a></div>
-                    ))}</div>
-                    <div id="footer-buttons">
-                        <ButtonSelectorLanguages items={data["servs-and-legal-terms"].prices} hasFlags="false" />
-                        <ButtonSelectorLanguages items={data["servs-and-legal-terms"].langs} hasFlags="true" />
-                    </div>
-                </div>
+  return (
+    <div id="servs-and-legal">
+      {data && (
+        <section id="servs-and-legal-section">
+          <div>
+            <a href="" title={data["servs-and-legal-terms"]["ea-icon"]}>
+              <img
+                id="ea-violet"
+                src="./src/home_page/media/img/footer/ea-violet.png"
+                alt="ea-logo"
+              />
+            </a>
+          </div>
+          <div id="ea-servs-and-legal">
+            <div>
+              <div id="servs-links">
+                {data["servs-and-legal-terms"].servs.map((link) => (
+                  <div className="servs-link" key={link}>
+                    <a href="">{link}</a>
+                  </div>
+                ))}
+              </div>
+              <div id="footer-buttons">
+                <ButtonSelectorLanguages
+                  items={data["servs-and-legal-terms"].prices}
+                  hasFlags="false"
+                />
+                <ButtonSelectorLanguages
+                  items={data["servs-and-legal-terms"].langs}
+                  hasFlags="true"
+                />
+              </div>
+            </div>
 
             <div>
               <div id="legal-terms">
