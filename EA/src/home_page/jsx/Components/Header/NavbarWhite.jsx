10fdc679f3faf4b27d47fc6dd125c2c9629d688d
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-04-09 15:16:10 +0200

    Task 40: Css fixes

--- EA/src/home_page/jsx/Components/Header/NavbarWhite.jsx
+++ EA/src/home_page/jsx/Components/Header/NavbarWhite.jsx
@@ -3,6 +3,7 @@ import Dropdown from "./Dropdown";
 import dots from "/src/home_page/media/img/svg/dropdowndots.svg";
 import anvorgesa from "/src/home_page/media/img/svg/anvorgesa.svg";
 import eaimage from "/src/home_page/media/img/svg/ealogo.svg";
+import DropdownBig from "./DropdownBig";
 
 export default function NavbarWhite({ openSidebar, openSidebarMedia, menu }) {
   return (
@@ -17,13 +18,26 @@ export default function NavbarWhite({ openSidebar, openSidebarMedia, menu }) {
         customClick={openSidebarMedia}
         id="f_icon_anvorgesa"
         src={anvorgesa}
-        srcClass="f_svg_drop"
+        srcClass="f_svg_drop f_anvorgesa"
       />
-      <IconButton id={"f_eaimage"} src={eaimage} aHref={""} />
+      <IconButton
+        id={"f_eaimage"}
+        src={eaimage}
+        aHref={""}
+        srcClass={"f_eaimage_src"}
+      />
 
-      {menu?.map((element, index) => (
-        <Dropdown header={element.title} itemList={element.list} key={index} />
-      ))}
+      {menu?.map((element, index) =>
+        index === 0 ? (
+          <DropdownBig item={element} key={index} />
+        ) : (
+          <Dropdown
+            header={element.title}
+            itemList={element.list}
+            key={index}
+          />
+        )
+      )}
     </div>
   );
 }
